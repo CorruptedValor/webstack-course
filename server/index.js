a7c96463b2443b3c2a7ed1857509b98c90801d17
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-07-24 01:01:37 -0700

    testing npm install error

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send({
-    hi: 'there',
-    bye: 'buddy'
-  });
+    hi: "there",
+    bye: "buddy"
+    });
 });
 
 app.listen(PORT);
