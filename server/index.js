f1bc81513fd7485f410be5e3116191ab20d92180
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-08-02 20:54:04 -0700

    oauth v2

--- server/index.js
+++ server/index.js
@@ -1,23 +1,34 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: "there",
-    bye: "buddy"
-    });
-});
+require('./routes/authroutes')(app);
+
 
 app.listen(PORT);
 
 console.log(`Listening on ${PORT}`);
 
-
-//deployment insturctions: use heroku
-//heroku login if needed. Don't think it is.
-//heroku create
-//git remote add heroku {link}
-//git push heroku master
-
-//additional deployments: git push heroku master
+//run npm install --save cookie-session
