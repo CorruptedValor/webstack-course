e9d00256d3667db02fa01fcb9c5c650566ac140d
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-08-13 20:40:04 -0700

    =Addind stripe functionality

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 require('./models/User');
@@ -10,6 +11,8 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -24,7 +27,8 @@ app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 
-require('./routes/authroutes')(app);
+require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 
 app.listen(PORT);
