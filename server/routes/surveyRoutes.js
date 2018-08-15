85b1b3f0a215482f443078614a801c00f4c8365b
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-08-15 00:51:57 -0700

    added front end email form

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -24,15 +24,16 @@ module.exports = app => {
         });
 
         const mailer = new Mailer(survey, surveyTemplate(survey));
+
         try{
             await mailer.send();
             await survey.save();
             req.user.credits -= 1;
             const user = await req.user.save();
+            
+            res.send(user);
         } catch (err) {
             res.status(422).send(err);
         }
-
-        res.send(user);
     });
 };
