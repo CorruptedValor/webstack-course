c00b7cceccbbf8f791728df10590fb0daf795212
Jeffrey McKinley <corruptedvalor@gmail.com>
2018-08-15 15:29:10 -0700

    Dashboard display implemented

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -12,14 +12,14 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
     app.get('/api/surveys', requireLogin, async (req, res) => {
         const surveys = await Survey.find({ _user: req.user.id })
-            .select()
+            .select({ recipients:false });
 
-        res.send(surveys);
-    })
+        res.send(surveys)
+    });
 
     app.get('/api/surveys/:surveyId/:choice', (req,res) => {
         res.send('Thanks for voting!');
-    })
+    });
 
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
